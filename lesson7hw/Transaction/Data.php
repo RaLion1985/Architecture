63ee519a081b76729472870c0415215435b32ce8
RaLion1985 <y-shkuro@mail.ru>
2020-11-17 13:11:09 +0300

    Домашнее задание к 7 уроку

---
+++ lesson7hw/Transaction/Data.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Data
+{
+    public $name;
+    public $sum;
+
+
+    public function __construct($name, $sum)
+    {
+        $this->name = $name;
+        $this->sum = $sum;
+    }
+
+}
